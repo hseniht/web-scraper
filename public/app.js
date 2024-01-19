1ae6a9dd81ce51e3859a7694633672abf973c9d6
hseniht <thinesh_kumar_mu@hotmail.com>
2024-01-19 11:50:06 +0800

    Scrap Metadata data functionalities

--- public/app.js
+++ public/app.js
@@ -1,5 +1,21 @@
 // public/app.js
 
+function updateMetadata(metadata) {
+  document.getElementById("title").textContent =
+    "Title: " + (metadata.title || "Not found");
+  document.getElementById("description").textContent =
+    "Description: " + (metadata.description || "Not found");
+  document.getElementById("keywords").textContent =
+    "Keywords: " + (metadata.keywords || "Not found");
+  document.getElementById("author").textContent =
+    "Author: " + (metadata.author || "Not found");
+  document.getElementById("image").textContent =
+    "Image: " + (metadata.image || "Not found");
+  document.getElementById("favicon").textContent =
+    "Favicon: " + (metadata.favicon || "Not found");
+  document.getElementById("url").textContent = "URL: " + metadata.url;
+}
+
 document.getElementById("myButton").addEventListener("click", async () => {
   console.log("tk html button clicked 2");
   try {
@@ -7,6 +23,7 @@ document.getElementById("myButton").addEventListener("click", async () => {
     const response = await fetch("/api/data"); // Assumes your server is running on the same domain
     const data = await response.json();
     console.log("tk resp data", data);
+    updateMetadata(data.metaData);
     document.getElementById("message").innerText = JSON.stringify(data);
   } catch (error) {
     console.error(error);
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const cheerio = require("cheerio");
 const app = express();
 const port = 3000;
+const url = require("url");
 
 // Serve static files from the "public" directory
 app.use(express.static("../public"));
@@ -16,12 +17,14 @@ function escapeColons(selector) {
 
 // Handle requests to the root URL
 app.get("/api/data", async (req, res) => {
-  const url =
+  const targetUrl =
     "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html";
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(targetUrl);
     const html = response.data;
+
+    // Parse HTML using Cheerio
     const $ = cheerio.load(html);
 
     const selector = "h1";
@@ -29,7 +32,27 @@ app.get("/api/data", async (req, res) => {
     const modifiedSelector = escapeColons(selector);
 
     const scrapedData = $(selector, html).text();
-    res.json(scrapedData);
+
+    // Function to resolve relative URLs to absolute URLs
+    function resolveRelativeUrl(baseUrl, relativeUrl) {
+      return url.resolve(baseUrl, relativeUrl);
+    }
+
+    faviconRelativeUrl = $('link[rel="icon"]').attr("href");
+    const faviconUrl = faviconRelativeUrl
+      ? resolveRelativeUrl(targetUrl, faviconRelativeUrl)
+      : null;
+
+    const basicMeta = {
+      title: $("head title").text(),
+      description: $('meta[name="description"]').attr("content"),
+      keywords: $('meta[name="keywords"]').attr("content"),
+      author: $('meta[name="author"]').attr("content"),
+      image: $('meta[property="og:image"]').attr("content"),
+      favicon: faviconUrl,
+      url: targetUrl,
+    };
+    res.json({ scrapedData: scrapedData, metaData: basicMeta });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
